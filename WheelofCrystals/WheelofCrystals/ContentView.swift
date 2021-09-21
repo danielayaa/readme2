bac5cffa57358f3b5decde393dcc720cd0ef86b3
danielayaa <daniela1001@student.k12.tulsaschools.org>
2021-09-21 09:22:59 -0500

    Added carosel effect

--- WheelofCrystals/WheelofCrystals/ContentView.swift
+++ WheelofCrystals/WheelofCrystals/ContentView.swift
@@ -12,25 +12,45 @@ struct ContentView: View {
     @State var selected: Int = 0
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack (alignment: .center){
+            Text ("Wheel of Crystals")
+                .font(.system(size: 45))
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .padding(.top, 50)
         
         TabView(selection: $selected) {
             // images
             ForEach(1...4, id: \.self) { index in
                 
-                Image("p\(index)")
-                    .resizable()
-                    .frame(width: 400.0, height: 400.0)
-                    .aspectRatio(contentMode:
-                                    .fill)
+                ZStack {
+                    GeometryReader { reader in
+                        Image("p\(index)")
+                            .resizable()
+                            .frame(width:400.0, height: 400.0)
+                            .aspectRatio(contentMode:
+                                        .fill)
+                            .offset(x: -reader.frame(in:.global).minX)
+                    }
+                    .frame(height: 400.0)
+                    .cornerRadius(15)
+                    .padding(10)
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(color: Color.black.opacity(0.2),radius: 5, x:5, y:5)
+                    .shadow(color: Color.black.opacity(0.2),radius: 5, x:-5, y:5)
+                    .padding(.horizontal, 25)
+                }
             }
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
     }
-}
+  }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+  }
 }
