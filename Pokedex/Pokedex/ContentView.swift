40841636b51f660fd34b30bc31c50724ff013589
danielayaa <daniela1001@student.k12.tulsaschools.org>
2021-10-19 09:13:23 -0500

    pokemon card layout is done

--- Pokedex/Pokedex/ContentView.swift
+++ Pokedex/Pokedex/ContentView.swift
@@ -10,18 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            VStack (alignment: .leading){
+            VStack (alignment: .leading) {
                 Text("Bulbasaur")
                     .font(.title2)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .padding(.top, 10)
+                    .padding(.leading)
                 
                 HStack {
                     Text("Poison/Grass")
                         .font(.caption)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
                     
+                        .overlay(RoundedRectangle (cornerRadius: 12)
+                        .fill(Color.white.opacity(0.25))
+                    )
+                    
+                        .frame(width: 100, height: 25)
+                                 
                     Image("Bulb")
                         .resizable()
                         .scaledToFit()
@@ -32,6 +42,7 @@ struct ContentView: View {
         }
         .background(Color.green)
         .cornerRadius(12)
+        .shadow(color: .green, radius: 8, x: 0, y: 0)
     }
 }
 
