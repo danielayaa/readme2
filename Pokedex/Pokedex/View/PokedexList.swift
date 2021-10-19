5bf3b4d388774ad5fe09b52798a61d84157fc3d8
danielayaa <daniela1001@student.k12.tulsaschools.org>
2021-10-19 09:24:46 -0500

    made pokedexlist view

---
+++ Pokedex/Pokedex/View/PokedexList.swift
@@ -0,0 +1,24 @@
+//
+//  PokedexList.swift
+//  Pokedex
+//
+//  Created by AMStudent on 10/19/21.
+//
+
+import SwiftUI
+
+struct PokedexList: View {
+    
+    private let gridItems = [GridItem(.flexible()),
+                GridItem(.flexible())]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokedexList_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexList()
+    }
+}
