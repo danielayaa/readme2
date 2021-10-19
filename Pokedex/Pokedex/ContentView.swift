f39e9632b544287776f519610c0b3b0eb02a5ca3
danielayaa <daniela1001@student.k12.tulsaschools.org>
2021-10-19 08:56:40 -0500

    Made app/started

---
+++ Pokedex/Pokedex/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by AMStudent on 10/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Bulbasaur")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Text("Poison/Grass")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Image("Bulb")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 68, height: 68)
+                        .padding([.bottom, .trailing], 5)
+                }
+            }
+        }
+        .background(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
